447cb03de6620142931c2d4d80b4a79d632fbf33
Marcos Luiz Mittag Filho <marquinhoszx_xd@outlook.com>
2018-06-14 16:08:01 -0300

    repository

--- WindowsFormsApplication1/CadastroYT.cs
+++ WindowsFormsApplication1/CadastroYT.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Model;
 using System.Collections.Generic;
+using System.IO;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -8,11 +9,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace WindowsFormsApplication1
 {
     public partial class Cadastro : Form
     {
+        public static string NOME_ARQUIVO = "Youtuber.bin";
+        public int posicao = -1;
         public Cadastro()
         {
             InitializeComponent();
@@ -171,39 +175,21 @@ namespace WindowsFormsApplication1
                 MessageBox.Show("Quantidade De strikes deve ser preenchido");
 
             }
-            
-            
-            
-            
-            //try
-            //{
 
-            //    Youtuber youtuber = new Youtuber();
-            //    youtuber.SetNome(txtNome.Text);
-            //    youtuber.SetApelido(txtApelido.Text);
-            //    youtuber.SetSobrenome(textBox1.Text);
-            //    youtuber.SetQuantidadeDeInscritos(Convert.ToInt32(txtTotalInscritos.Text));
-            //    youtuber.SetQuantidadeDeLikes(Convert.ToInt32(txtQuantidadeDeLikes.Text));
-            //    youtuber.SetQuantidadeDeViews(Convert.ToInt64(txtQuantidadeDeViews.Text));
-            //    youtuber.SetDescricao(textBox3.Text);
-            //    youtuber.SetRenda(Convert.ToInt64(txtRenda.Text));
-            //    youtuber.SetLink(txtLink.Text);
-            //    youtuber.SetNacionalidade(txtNacionalidade.Text);
-            //    youtuber.SetQuantidadeDeVideos(Convert.ToInt32(numericUpDown1.Value));
-            //    youtuber.SetCategoria(txtCategoria.Text);
-            //    youtuber.SetAnuncio(Convert.ToBoolean(cbSimAnuncio.Text));
-            //    youtuber.SetAnuncio(Convert.ToBoolean(cbNaoAnuncio.Text));
-            //    youtuber.SetPatrocinador(Convert.ToBoolean(cbSimAnuncio.Text));
-            //    youtuber.SetPatrocinador(Convert.ToBoolean(cbNaoPatrocinador.Text));
-            //    youtuber.SetQuantidadeStrikes(Convert.ToInt32(nudQuantidadeStrikes.Value));
-            //    youtuber.SetStreamer(Convert.ToBoolean(cbSimStreamer.Text));
-            //    youtuber.SetStreamer(Convert.ToBoolean(cbNãoStreamer.Text));
 
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message);
-            //}
+            YoutuberRepository tudo = new YoutuberRepository();
+            if (posicao == -1)
+            {
+                tudo.AdicionarYoutuber(youtuber);
+                MessageBox.Show("Youtuber Cadastrado com sucesso");
+            }
+            else
+            {
+                tudo.EditarYoutuber(youtuber, posicao);
+                MessageBox.Show("Youtuber alterado com sucesso");
+            }
+
+
         }
 
         
--- WindowsFormsApplication1/YoutuberRepository.cs
+++ WindowsFormsApplication1/YoutuberRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +11,51 @@ namespace WindowsFormsApplication1
 {
     class YoutuberRepository
     {
-        List<Youtuber> youtuber = new List<Youtuber>();
+        List<Youtuber> youtuberes = new List<Youtuber>();
         public YoutuberRepository()
         {
-            
+            if (File.Exists(Cadastro.NOME_ARQUIVO))
+            {
+                BinaryFormatter binaryReader = new BinaryFormatter();
+                Stream stream = File.OpenRead(Cadastro.NOME_ARQUIVO);
+                youtuberes = ((YoutuberRepository)binaryReader.Deserialize(stream)).ObterYoutuber();
+                stream.Close();
+            }
+        }
 
+        private List<Youtuber> ObterYoutuber()
+        {
+            return youtuberes;
+        }
+
+        internal void AdicionarYoutuber(Youtuber youtuber)
+        {
+            youtuberes.Add(youtuber);
+            EscreverNoArquivoDosYoutubers();
+        }
+        private void EscreverNoArquivoDosYoutubers()
+        {
+            BinaryFormatter binaryWriter = new BinaryFormatter();
+            Stream stream = new FileStream(Cadastro.NOME_ARQUIVO, FileMode.Create, FileAccess.Write);
+            binaryWriter.Serialize(stream, this);
+            stream.Close();
+        }
+        internal void apagarYoutuber(string nome)
+        {
+            foreach (Youtuber youtuber in youtuberes)
+            {
+                if (youtuber.GetNome() == nome)
+                {
+                    youtuberes.Remove(youtuber);
+                    EscreverNoArquivoDosYoutubers();
+                    return;
+                }
+            }
+        }
+        internal void EditarPersonagem(Youtuber youtuber, int posicao)
+        {
+            youtuberes[posicao] = youtuber;
+            EscreverNoArquivoDosYoutubers();
         }
     }
 }
